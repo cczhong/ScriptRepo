86b02bbfffbe6454354c2410e7488f6f72f4f0e1
cczhong <cuncongzhong@gmail.com>
2015-10-13 09:30:40 -0700

    ComputeN50.pl: consider multi-line FASTA file.

--- ComputeN50.pl
+++ ComputeN50.pl
@@ -26,14 +26,17 @@ if($N <= 0 || $N >= 100)  {
 
 my $total_length = 0;
 my @lengths;
+my $accu_len = 0;
 open my $IN, "<$fasta_file" or die "Cannot open file: $!\n";
 while(<$IN>)  {
   chomp;
   if(/^\>/)  {
+    push @lengths, $accu_len;
+    $total_length += $accu_len;
+    $accu_len = 0;
     next;
   }
-  push @lengths, length($_);
-  $total_length += length($_);
+  $accu_len += length($_);
 }
 close $IN;
 
